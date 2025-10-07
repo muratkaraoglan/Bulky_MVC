e5515f7efc14d1ffcceb3b4ece0f22e1dad1bc53
Murat Karaoglan <m.karaoglan1604@gmail.com>
2025-10-07 23:30:51 +0300

    Category page

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var objCategories = _db.Categories.ToList();
+        return View(objCategories);
+    }
+}
