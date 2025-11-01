14bb59caac182774c87d60da8189e55c2389cd8c
Murat Karaoglan <m.karaoglan1604@gmail.com>
2025-11-01 14:42:34 +0300

    Create and Update products

--- Bulky.DataAccess/Repository/ProductRepository.cs
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -15,6 +15,20 @@ public class ProductRepository : Repository<Product>, IProductRepository
 
     public void Update(Product product)
     {
-        _db.Products.Update(product);
+        var productFromDb = _db.Products.FirstOrDefault(p => p.Id == product.Id);
+        if (productFromDb != null)
+        {
+            productFromDb.Title = product.Title;
+            productFromDb.ISBN = product.ISBN;
+            productFromDb.Price = product.Price;
+            productFromDb.Price50 = product.Price50;
+            productFromDb.Price100 = product.Price100;
+            productFromDb.ListPrice = product.ListPrice;
+            productFromDb.Description = product.Description;
+            productFromDb.CategoryId = product.CategoryId;
+            productFromDb.Author = product.Author;
+            if(product.ImageUrl != null) productFromDb.ImageUrl = product.ImageUrl;
+        }
+       
     }
 }
--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Bulky.Models.ViewModels;
@@ -10,10 +11,12 @@ namespace BulkyWeb.Areas.Admin.Controllers;
 public class ProductController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IWebHostEnvironment _webHostEnvironment;
 
-    public ProductController(IUnitOfWork unitOfWork)
+    public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
     {
         _unitOfWork = unitOfWork;
+        _webHostEnvironment = webHostEnvironment;
     }
 
     // GET
@@ -25,7 +28,7 @@ public class ProductController : Controller
         return View(products);
     }
 
-    public IActionResult Create()
+    public IActionResult Upsert(int? id) //Update Insert
     {
         IEnumerable<SelectListItem> categories = _unitOfWork.Category
             .GetAll().Select(c => new SelectListItem
@@ -38,35 +41,52 @@ public class ProductController : Controller
             Product = new Product(),
             CategoryList = categories
         };
-        return View(productVm);
+        if (id == null || id == 0) // Insert
+        {
+            return View(productVm);
+        }
+        else //Update
+        {
+            productVm.Product = _unitOfWork.Product.Get(p => p.Id == id);
+            return View(productVm);
+        }
     }
 
     [HttpPost]
-    public IActionResult Create(ProductVM productVm)
+    public IActionResult Upsert(ProductVM productVm, IFormFile? file)
     {
         if (!ModelState.IsValid) return View();
-        _unitOfWork.Product.Add(productVm.Product);
+        if (file != null)
+        {
+            var wwwRootPath = _webHostEnvironment.WebRootPath;
+            var fileName = Guid.NewGuid() + file.FileName;
+            var productPath = Path.Combine(wwwRootPath, @"images\product");
+
+            if (!string.IsNullOrEmpty(productVm.Product.ImageUrl))
+            {
+                //delete the old image
+                var oldImagePath = Path.Combine(wwwRootPath, productVm.Product.ImageUrl.TrimStart('\\'));
+                if (System.IO.File.Exists(oldImagePath)) System.IO.File.Delete(oldImagePath);
+            }
+
+            using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+            productVm.Product.ImageUrl = @"\images\product\" + fileName;
+        }
+
+        if (productVm.Product.Id == 0)
+        {
+            _unitOfWork.Product.Add(productVm.Product);
+        }
+        else
+        {
+            _unitOfWork.Product.Update(productVm.Product);
+        }
+
         _unitOfWork.Save();
         TempData["Success"] = "Product created successfully";
-        return RedirectToAction("Index");
-    }
-
-    public IActionResult Edit(int? id)
-    {
-        if (id is null or 0) return NotFound();
-        var productFromDb = _unitOfWork.Product.Get(p => p.Id == id);
-        if (productFromDb is null) return NotFound();
-
-        return View(productFromDb);
-    }
-
-    [HttpPost]
-    public IActionResult Edit(Product product)
-    {
-        if (!ModelState.IsValid) return View();
-        _unitOfWork.Product.Update(product);
-        _unitOfWork.Save();
-        TempData["Success"] = "Product updated successfully";
         return RedirectToAction("Index");
     }
 
